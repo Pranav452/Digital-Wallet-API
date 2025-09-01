782aea9280cce25f1bd0d83ebd5caeb5d7412c8e
Pranav Nair <pranavnairop090@gmail.com>
2025-09-01 17:29:59 +0530

    wowowowow

--- app/routers/User_Management.py
+++ app/routers/User_Management.py
@@ -1,9 +1,8 @@
 #this file contains the routers for the user management system
 
 from fastapi import APIRouter
-from app.crud.User_Management import get_user, update_user, create_user
+from app.crud.User_Management import get_user, update_user
 
 router = APIRouter()
-router.post("/users")(create_user)
 router.get("/users/{user_id}")(get_user)
 router.put("/users/{user_id}")(update_user)
